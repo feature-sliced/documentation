27a6acfea483ed545b75e8fd7e470f406b5c4dc3
Ilya Azin <martis.azin@gmail.com>
2021-05-16 14:50:14 +0300

    feat(web): customize docusaurus config (main)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,31 +1,35 @@
+const DOMAIN = 'https://feature-sliced.design/';
+const GITHUB_ORG = 'https://github.com/feature-sliced/wiki'
+const GITHUB_DOCS = 'https://github.com/feature-sliced/wiki';
+
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'feature-sliced',
+  tagline: 'Методология для проектирования frontend проектов, нацеленная на разделение приложения согласно бизнес-логике и областям ответственности.',
+  url: 'https://github.com/feature-sliced/wiki',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'feature-sliced', // Usually your GitHub org/user name.
+  projectName: 'wiki', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'feature-sliced',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'logo',
+        // FIXME: Сделать под SVG позднее
+        src: 'img/logo.png',
       },
       items: [
         {
           type: 'doc',
-          docId: 'intro',
+          docId: 'readme',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Документация',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: GITHUB_DOCS,
           label: 'GitHub',
           position: 'right',
         },
@@ -38,8 +42,8 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Документация',
+              to: '/docs/readme',
             },
           ],
         },
@@ -47,34 +51,36 @@ module.exports = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'Telegram',
+              href: 'https://t.me/feature_sliced',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: 'Open Collective',
+              href: 'https://opencollective.com/feature-sliced',
             },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
+            // TODO: Добавить ссыль на twitter позднее (как доработаем)
+            // {
+            //   label: 'Twitter',
+            //   href: 'https://twitter.com/docusaurus',
+            // },
           ],
         },
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // TODO: Добавить ссыль на dev.to позднее (как доработаем)
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: GITHUB_ORG,
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()}  Feature-Sliced`,
     },
   },
   presets: [
@@ -86,14 +92,14 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            `${GITHUB_DOCS}/edit/master/website/`,
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     `${GITHUB_DOCS}/edit/master/website/blog/`,
+        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
