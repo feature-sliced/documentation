cc5cd8c8aa918b2dcb9ad239bddaa7b787961f94
Ilya Azin <martis.azin@gmail.com>
2021-10-23 02:32:49 +0300

    feat(feedback): add forms for both locales

--- website/src/features/feedback/index.jsx
+++ website/src/features/feedback/index.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { FlagOutlined } from "@ant-design/icons";
-// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./styles.module.css";
 
+const FEEDBACK_URLS = {
+    ru: "https://forms.gle/7p4anU2shHAzmfqc8",
+    en: "https://forms.gle/nsYua6bMMG5iBB3v7",
+};
+
+const FEEDBACK_LABEL = {
+    ru: "Поделись фидбеком по документации 🤙",
+    en: "Share your feedback about documentation 🤙",
+};
+
 export const Feedback = () => {
-    // TODO: implement for both locales
-    // const { siteConfig } = useDocusaurusContext();
+    const { i18n } = useDocusaurusContext();
     return (
         <div className={styles.root}>
             <a
                 className={styles.button}
-                href="https://forms.gle/7p4anU2shHAzmfqc8"
+                href={FEEDBACK_URLS[i18n.currentLocale]}
                 target="_blank"
                 rel="noreferrer noopener"
+                title={FEEDBACK_LABEL[i18n.currentLocale]}
             >
                 <FlagOutlined className={styles.icon} />
             </a>
