8a47a858efdd3ee9a92ec51bc2feb284958f2974
Ilya Azin <martis.azin@gmail.com>
2021-10-31 19:52:05 +0300

    FEEDBACK-309: @fix(docs-feedback): minor fix

--- website/src/theme/DocItemFooter/index.js
+++ website/src/theme/DocItemFooter/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+// eslint-disable-next-line import/no-unresolved
 import OriginalDocItemFooter from "@theme-original/DocItemFooter";
 import { DocFeedback } from "@site/src/features/feedback/doc";
 import styles from "./styles.module.css";
 
 /**
  * DocItemFooter
- * @swizzled unsafe: higher breaking change risk
- * @remark Keep actual!
- * TODO: describe coupling to original
+ * @swizzled unsafe: higher breaking change risk (keep actual!)
+ * @remark Couple to original DocItemFooter for more stability
+ * @see https://docusaurus.io/docs/next/using-themes#for-site-owners
  */
 function DocItemFooter(props) {
     return (
