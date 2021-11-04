14821c29de8608f57ee68d6e2d0262cb1db75898
Ilya Azin <martis.azin@gmail.com>
2021-11-04 10:32:52 +0300

    DISCOVERY-332: @feat(restruct): add meta-tags for rows

--- website/src/shared/ui/row/index.jsx
+++ website/src/shared/ui/row/index.jsx
@@ -6,13 +6,17 @@ import styles from "./styles.module.css";
  * Row card for linking
  * @see https://docusaurus.io/docs/next/markdown-features/react#importing-markdown
  */
-export const Row = ({ title, description, to, Icon }) => {
+export const Row = (props) => {
+    const { title, description, to, Icon, tags } = props;
     return (
         <Link className={styles.root} to={to}>
             <RowIcon Icon={Icon} />
             <div className={styles.details}>
-                <span className={styles.title}>{title}</span>
-                <p className={styles.description}>{description}</p>
+                <div className={styles.detailsMain}>
+                    <span className={styles.title}>{title}</span>
+                    <p className={styles.description}>{description}</p>
+                </div>
+                {tags && <div className={styles.detailsTags}>{tags.join(" • ")}</div>}
             </div>
         </Link>
     );
