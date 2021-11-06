922adf5b6b8fd6b92f4d261c3233ddd438768319
Ilya Azin <martis.azin@gmail.com>
2021-11-06 23:24:28 +0300

    chore(feedback): add metrics for NavRow

--- website/src/shared/ui/row/index.jsx
+++ website/src/shared/ui/row/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useCallback } from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
+
+import { ga } from "@site/src/shared/lib/ga";
 import styles from "./styles.module.css";
 
 /**
@@ -9,8 +11,16 @@ import styles from "./styles.module.css";
  */
 export const Row = (props) => {
     const { title, description, to, Icon, tags, className } = props;
+    const handleClick = useCallback(() => {
+        ga.sendEvent({
+            category: ga.CATEGORIES.full,
+            action: "NavRow:Click",
+            label: to,
+        });
+    }, [to]);
+
     return (
-        <Link className={clsx(styles.root, className)} to={to}>
+        <Link className={clsx(styles.root, className)} to={to} onClick={handleClick}>
             <RowIcon Icon={Icon} />
             <div className={styles.details}>
                 <div className={styles.detailsMain}>
