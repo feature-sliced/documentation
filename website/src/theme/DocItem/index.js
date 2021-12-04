b2b1980fd55f032616a2d2a58fccefdbdccff025
Evgeniy <daem0nbfg@gmail.com>
2021-12-04 15:35:13 +0300

    fix: meta url and locale info from DocusaurusContext. wrong path for not default locales.

--- website/src/theme/DocItem/index.js
+++ website/src/theme/DocItem/index.js
@@ -1,21 +1,23 @@
 import sha1 from "sha1";
 import React from "react";
 import OriginalDocItem from "@theme-original/DocItem";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { OpenGraphPreview } from "@site/src/shared/lib/open-graph-preview";
 
 function DocItem(props) {
     const { content } = props;
-    const { id, source } = content.metadata;
+    const { id } = content.metadata;
+    const { siteConfig, i18n } = useDocusaurusContext();
 
-    const { locale } = source.match(/i18n\/(?<locale>[a-z]+)\//)?.groups;
-
-    const hashFileName = sha1(id + (locale || "en"));
+    const hashFileName = sha1(id + i18n.currentLocale);
 
     return (
         <>
-            {/* TODO: Add and use domain from env? */}
             <OpenGraphPreview
-                imgUrl={`https://feature-sliced.design/assets/og/${hashFileName}.jpg`}
+                imgUrl={`${siteConfig.url}${
+                    /* OG Preview images build with locale prefix (.../en/assets/...) for not default locales */
+                    i18n.currentLocale !== i18n.defaultLocale ? `/${i18n.currentLocale}` : ""
+                }/assets/og/${hashFileName}.jpg`}
             />
             <OriginalDocItem {...props} />
         </>
