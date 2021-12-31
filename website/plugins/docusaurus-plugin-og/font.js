44d73297355fffb25434eda61b71757a1afd8088
Krakazybik <daem0nbfg@gmail.com>
2021-12-31 22:18:15 +0300

    revert(og-font): deleted font-autoscaling and font-align-center.

--- website/plugins/docusaurus-plugin-og/font.js
+++ website/plugins/docusaurus-plugin-og/font.js
@@ -23,24 +23,10 @@ function createSVGText(
     const attributes = { fill, stroke };
     const options = { fontSize, anchor: "top", attributes };
     // Remove all emoji from text
-    let filteredText = text.replace(
+    const filteredText = text.replace(
         /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
         "",
     );
-
-    /* If font width more than widthLimit => scale font width to ~90% of widthLimit */
-    if (widthLimit) {
-        const { width } = font.getMetrics(filteredText, options);
-        if (width > widthLimit)
-            options.fontSize = Math.trunc((fontSize * 0.9) / (width / widthLimit));
-        else {
-            // Center text using spaces.
-            const { width: spaceWidth } = font.getMetrics(" ", options);
-            const availableSpacesCount = Math.trunc((widthLimit - width) / spaceWidth / 2);
-            const spaces = " ".repeat(availableSpacesCount);
-            filteredText = `${spaces}${filteredText}${spaces}`;
-        }
-    }
 
     return font.getSVG(filteredText, options);
 }
