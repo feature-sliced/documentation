9d5ea0f1d386e0a2969228eeaa3ff558df46add7
Krakazybik <daem0nbfg@gmail.com>
2021-12-31 22:54:22 +0300

    refactor(docusaurus-doc): added variable build path for OG.

--- website/src/theme/DocItem/index.js
+++ website/src/theme/DocItem/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import OriginalDocItem from "@theme-original/DocItem";
 import { OGMeta } from "@site/src/shared/lib/og-meta";
-import { useOGUrl } from "./lib";
+import { useDocOGUrl } from "./lib";
 
 function DocItem(props) {
     const { content } = props;
-    const ogUrl = useOGUrl(content.metadata);
+    const ogUrl = useDocOGUrl(content.metadata);
 
     return (
         <>
--- website/src/theme/DocItem/lib/index.js
+++ website/src/theme/DocItem/lib/index.js
@@ -1,12 +1,12 @@
 import sha1 from "sha1";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-export function useOGUrl({ id }) {
+export function useDocOGUrl(metadata, buildOGPath = "assets/og/") {
     const { siteConfig, i18n } = useDocusaurusContext();
-    const hashFileName = sha1(id + i18n.currentLocale);
+    const hashFileName = sha1(metadata.id + i18n.currentLocale);
 
     /* OG Preview images build with locale prefix (.../en/assets/...) for not default locales */
     return `${siteConfig.url}${
         i18n.currentLocale !== i18n.defaultLocale ? `/${i18n.currentLocale}` : ""
-    }/assets/og/${hashFileName}.jpg`;
+    }/${buildOGPath}${hashFileName}.jpg`;
 }
