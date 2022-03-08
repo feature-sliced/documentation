1628ed6bcdeab2cd390785b1f2124eccfe3e5374
itis-ani <saratriplesara@gmail.com>
2022-03-08 13:37:37 +0300

    chore: update banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -283,8 +283,11 @@ const announcementBar = {
     // backgroundColor: '#e6a700', // As caution by docusaurus (defaults was `#fff`)
     // FIXME: (i18n) translate by locale later (how to?)
     // content: `If you are using the methodology <a href="/versions">(v0 / v1 / v2)</a> at work or in personal projects, <a href="https://github.com/feature-sliced/documentation/issues/131" target="_blank" rel="noreferrer noopener">tell, us!</a>`,
-    content: `📚 Documentation refinements are in progress. <a href="https://github.com/feature-sliced/documentation/issues/263" target="_blank" rel="noreferrer noopener">Stay tuned for updates</a> and <a href="https://forms.gle/nsYua6bMMG5iBB3v7" target="_blank" rel="noreferrer noopener">share your feedback</a>`,
-    backgroundColor: "#5c9cb5", // As primary theme
+    // content: `📚 Documentation refinements are in progress. <a href="https://github.com/feature-sliced/documentation/issues/263" target="_blank" rel="noreferrer noopener">Stay tuned for updates</a> and <a href="https://forms.gle/nsYua6bMMG5iBB3v7" target="_blank" rel="noreferrer noopener">share your feedback</a>`,
+    // backgroundColor: "#5c9cb5", // As primary theme
+    // backgroundColor: "#0367d2",
+    content: `☮️ Stop the war in Ukraine! #NoWar`, // #nowar
+    backgroundColor: "#000000", // #nowar
     textColor: "#fff", // Defaults to `#000`.
     isCloseable: false, // Defaults to `true`.
 };
