a986a35a551a7feef3e07f1abb65ab87c4cd0c7c
Ilya Azin <martis.azin@gmail.com>
2022-03-26 11:11:36 +0300

    PROMO: (brand) setup color-theme switching (persist)

--- src/features/devtools/model.ts
+++ src/features/devtools/model.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import cookies from "js-cookie";
 
 export const THEMES = {
     RED: "red",
@@ -8,19 +9,24 @@ export const THEMES = {
 
 export const THEMES_KEYS = Object.values(THEMES);
 
+const THEME_COOKIE = "COLOR_THEME";
+
+const defaultTheme = cookies.get(THEME_COOKIE) || THEMES.BLUE;
+
 /**
  * @variant "red"
  * @variant "green"
  * @variant "blue"
  */
 export function useSwitchTheme() {
-    const [theme, setTheme] = useState(THEMES.BLUE);
+    const [theme, setTheme] = useState(defaultTheme);
 
     const onToggle = () => {
         const idx = THEMES_KEYS.indexOf(theme);
         const nextIdx = (idx + 1) % THEMES_KEYS.length;
         const nextTheme = THEMES_KEYS[nextIdx];
         setTheme(nextTheme);
+        cookies.set(THEME_COOKIE, nextTheme);
     };
 
     return { theme, setTheme, onToggle };
