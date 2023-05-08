ee7d638bc5eaa67341bae77fdbb35cfb2e49b311
Ilya Azin <martis.azin@gmail.com>
2023-05-08 15:58:16 +0600

    feat(home): adapt design - ecosystem carousel

--- src/pages/_home/index.tsx
+++ src/pages/_home/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 import Layout from "@theme/Layout";
 // import Image from "@theme/IdealImage";
 // import { translate } from "@docusaurus/Translate";
@@ -7,7 +8,21 @@ import Layout from "@theme/Layout";
 // NOTE: Locate at index before of specific route-mapping while Node/SSR building
 // "_" for excluding file from routing
 // import { features, concepts } from "./_config";
+import {
+    GithubOutlined,
+    YoutubeFilled,
+    AppstoreAddOutlined,
+    ReadOutlined,
+    TwitterOutlined,
+    ApiOutlined,
+    SolutionOutlined,
+} from "@ant-design/icons";
+import { NavCard } from "@site/src/shared/ui";
 import { Hero } from "./hero";
+// FIXME: [FSDCUR] Replace to unified IBM/Carbon icons usage
+import IconNpm from "./npm.svg";
+import IconTelegram from "./telegram.svg";
+import IconDiscord from "./discord.svg";
 import { Companies } from "./companies";
 import styles from "./styles.module.scss";
 
@@ -30,16 +45,93 @@ export default function HomePage() {
                     <img src="/img/promo/tech.png" alt="themed--scheme" />
                 </section>
                 <section className={styles.cols}>
-                    <div className={styles.col}>
-                        <span className={styles.demoText}>
-                            Краткий рассказ про тулинг, богатую доку, примеры, статьи, чаты, ивенты
+                    <div className={clsx(styles.col, styles.demoText)}>
+                        {/* FIXME: [FSDCUR] refine layout */}
+                        <span>
+                            🍰 <b>Feature-Sliced Design</b> is not only a methodology, but also a
+                            whole <a href="/community">ecosystem</a>:
+                            <br />
+                            - documentation
+                            <br />
+                            - community platforms
+                            <br />
+                            - media resources
+                            <br />- tooling for working in projects
                         </span>
                     </div>
-                    <div className={styles.col}>
-                        <span className={styles.demoText}>
-                            Вертикальная карусель карточек с элементами из экосистемы (пример,
-                            статья, ивент)
-                        </span>
+                    <div className={clsx(styles.col, styles.ecosystem)}>
+                        <div className={styles.ecosystemList}>
+                            <NavCard
+                                theme="mini"
+                                Icon={ReadOutlined}
+                                title="Documentation"
+                                description="Completed and self-sufficient documentation with practical and theoretical aspects of FSD usage"
+                                to="/docs"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={ApiOutlined}
+                                title="Practical guides"
+                                description="Practice-oriented guidelines"
+                                to="/docs/guides"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={AppstoreAddOutlined}
+                                title="Showcase"
+                                description="List of realworld websites people are building with FSD"
+                                to="/showcase"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={IconNpm}
+                                title="@feature-sliced/eslint-config"
+                                description="Lint FSD concepts by existing eslint plugins"
+                                to="https://github.com/feature-sliced/eslint-config"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={SolutionOutlined}
+                                title="FSD Articles"
+                                description="Articles from the core-team and the community on the use of FSD in realworld projects"
+                                to="/blog"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={GithubOutlined}
+                                title="@feature-sliced/cra-template"
+                                description="CRA template with FSD app structurizing"
+                                to="https://github.com/feature-sliced/cra-template"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={YoutubeFilled}
+                                title="Public Talks"
+                                description="FSD YouTube channel with public talks and workshops"
+                                to="https://www.youtube.com/c/FeatureSlicedDesign"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={TwitterOutlined}
+                                title="#Media Twitter"
+                                description="Twitter profile with FSD news"
+                                to="https://twitter.com/feature_sliced"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={IconDiscord}
+                                title="#Media Discord"
+                                description="International discord community chat"
+                                to="https://discord.gg/S8MzWTUsmp"
+                            />
+                            <NavCard
+                                theme="mini"
+                                Icon={IconTelegram}
+                                title="#Media Telegram"
+                                description="RU telegram chat"
+                                to="https://t.me/feature_sliced"
+                            />
+                        </div>
                     </div>
                 </section>
                 <section className={styles.section} id="scheme-limitations">
@@ -55,36 +147,4 @@ export default function HomePage() {
             </main>
         </Layout>
     );
-}
-
-{
-    /* <Section title={translate({ id: "pages.home.features.title" })}>
-                    {features.map((feature) => (
-                        <Card key={feature.title} size={12 / features.length} {...feature} />
-                    ))}
-                </Section>
-                <Section title={translate({ id: "pages.home.concepts.title" })} withAltBg>
-                    {concepts.map((concept) => (
-                        <Card key={concept.title} size={12 / concepts.length} {...concept} />
-                    ))}
-                </Section>
-                <Section
-                    title={translate({ id: "pages.home.scheme.title" })}
-                    rowClass={styles.scheme}
-                > */
-}
-{
-    /**
-     *  NOTE: Set fixed height for correct alignment from mobile devices
-     *  @see https://t.me/c/1463227827/197935
-     *  NOTE: Cannot be used as native img because of ideal-image plugin preprocessing
-     */
-}
-{
-    /* <Image
-                        className={styles.schemeImg}
-                        img={imgScheme}
-                        alt="feature-sliced-scheme,themed--scheme"
-                    />
-                </Section> */
 }
