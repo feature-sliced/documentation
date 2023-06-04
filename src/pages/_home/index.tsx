bc185565440789e697ff39e6b2e862a0593906e6
Ilya Azin <martis.azin@gmail.com>
2023-06-04 16:36:43 +0600

    chore(home): lint

--- src/pages/_home/index.tsx
+++ src/pages/_home/index.tsx
@@ -36,34 +36,53 @@ export default function HomePage() {
         <Layout title="Welcome" description="Architectural methodology for frontend projects">
             <Hero />
             <main>
-                <section className={clsx(styles.section, styles.sectionText, styles.limitations)} id="scheme-limitations">
-                    <h2 className={styles.text}>Our aim is to cover the architectural needs of the <span className="text--primary">vast majority of front-end projects</span></h2>
-                    <br/>
+                <section
+                    className={clsx(styles.section, styles.sectionText, styles.limitations)}
+                    id="scheme-limitations"
+                >
+                    <h2 className={styles.text}>
+                        Our aim is to cover the architectural needs of the{" "}
+                        <span className="text--primary">vast majority of front-end projects</span>
+                    </h2>
+                    <br />
                     {/* FIXME: [FSDCUR] resolve color adaptive */}
                     <img src="/img/promo/limitations.png" alt="themed--scheme" />
                 </section>
                 {/* Граф с профитами от использования: чистый гит, параллелизация работы, связность и зацепленность, общая терминология итд */}
                 <section className={clsx(styles.section, styles.sectionText, styles.profits)}>
-                    <h2 className={styles.text}>Less <span className="text--red">pain</span>, more <span className="text--green">gain</span></h2>
+                    <h2 className={styles.text}>
+                        Less <span className="text--red">pain</span>, more{" "}
+                        <span className="text--green">gain</span>
+                    </h2>
                     <div className={styles.profictsList}>
                         <div className={styles.card}>
                             <div className={styles.cardImage}>
-                                <img src="/img/promo/coupling.png" alt='coupling--scheme' />
+                                <img src="/img/promo/coupling.png" alt="coupling--scheme" />
                             </div>
                             <div className={styles.cardContent}>
                                 <b>Low coupling, high cohesion</b>
                                 <p>You don’t need to remember what that means.</p>
-                                <p>Just know that it makes your code <span className="text--green">easier to unit-test</span> and refactoring is much more <span className="text--blue">predictable</span></p>
+                                <p>
+                                    Just know that it makes your code{" "}
+                                    <span className="text--green">easier to unit-test</span> and
+                                    refactoring is much more{" "}
+                                    <span className="text--blue">predictable</span>
+                                </p>
                             </div>
                         </div>
                         <div className={styles.card}>
                             <div className={styles.cardImage}>
-                                <img src="/img/promo/structure.png" alt='structure--scheme' />
+                                <img src="/img/promo/structure.png" alt="structure--scheme" />
                             </div>
                             <div className={styles.cardContent}>
                                 <b>Rigid, standardized structure</b>
                                 <p>New job where people use FSD? No stress!</p>
-                                <p>You’ll <span className="text--green">feel right</span> at home in the codebase. Get <span className="text--blue">straight to business</span>, skip the reinvented wheels.</p>
+                                <p>
+                                    You’ll <span className="text--green">feel right</span> at home
+                                    in the codebase. Get{" "}
+                                    <span className="text--blue">straight to business</span>, skip
+                                    the reinvented wheels.
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -165,24 +184,46 @@ export default function HomePage() {
                     <img src="/img/promo/example-units.jpeg" alt="themed--scheme" />
                 </section> */}
                 <section className={clsx(styles.section, styles.sectionText, styles.companies)}>
-                    <h2 className={styles.text}>It’s <span className="text--red">dangerous</span> to go alone</h2>
-                    <p>But you don’t have to. These companies are already using Feature-Sliced Design:</p>
+                    <h2 className={styles.text}>
+                        It’s <span className="text--red">dangerous</span> to go alone
+                    </h2>
+                    <p>
+                        But you don’t have to. These companies are already using Feature-Sliced
+                        Design:
+                    </p>
                     <Companies />
                 </section>
                 <section className={clsx(styles.section, styles.sectionText, styles.nowar)}>
-                    <h2 className={styles.text}>The FSD team stands with Ukraine <div className={styles.flag} /></h2>
-                    <p>The Feature-Sliced Design team unanimously despises the invasion into Ukraine led by the Russian government.</p>
-                    <p><span className="text--primary">Can and want to help?</span> The best way is to seek out a local organization that supports Ukrainian refugees in your country and help someone directly.</p>
+                    <h2 className={styles.text}>
+                        The FSD team stands with Ukraine <div className={styles.flag} />
+                    </h2>
+                    <p>
+                        The Feature-Sliced Design team unanimously despises the invasion into
+                        Ukraine led by the Russian government.
+                    </p>
+                    <p>
+                        <span className="text--primary">Can and want to help?</span> The best way is
+                        to seek out a local organization that supports Ukrainian refugees in your
+                        country and help someone directly.
+                    </p>
                 </section>
                 <section className={clsx(styles.section, styles.sectionText, styles.cta)}>
                     <h2 className={styles.text}>
                         Think FSD and you are a match? <span className="text--red">&lt;3</span>
                     </h2>
                     <div className={styles.ctaActions}>
-                        <KeyLink href="/docs" keyIcon={KeyLink.Enter} isActive>Dive into the docs</KeyLink>
-                        <KeyLink href="/docs" keyIcon="E">Discover ecosystem</KeyLink>
-                        <KeyLink href="/docs" keyIcon="S">Explore realworld examples</KeyLink>
-                        <KeyLink href="/docs" keyIcon="D">Talk to someone about it</KeyLink>
+                        <KeyLink href="/docs" keyIcon={KeyLink.Enter} isActive>
+                            Dive into the docs
+                        </KeyLink>
+                        <KeyLink href="/docs" keyIcon="E">
+                            Discover ecosystem
+                        </KeyLink>
+                        <KeyLink href="/docs" keyIcon="S">
+                            Explore realworld examples
+                        </KeyLink>
+                        <KeyLink href="/docs" keyIcon="D">
+                            Talk to someone about it
+                        </KeyLink>
                     </div>
                 </section>
             </main>
