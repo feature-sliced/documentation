6112f567a7d40a5abbbd24ac31b5d75c10ed7dd6
Ilya Azin <martis.azin@gmail.com>
2023-07-14 18:20:59 +0600

    FSDCUR: Promo landing refinements (#587)
    * feat(home): adapt design - impl and integrate KeyLink basely
    
    * feat(home): adapt design - CTA & nowar & companies
    
    * feat(home): adapt design - minor refinements (profits)
    
    * chore(home): lint

--- src/pages/_home/index.tsx
+++ src/pages/_home/index.tsx
@@ -18,7 +18,7 @@ import {
     SolutionOutlined,
     DeploymentUnitOutlined,
 } from "@ant-design/icons";
-import { NavCard } from "@site/src/shared/ui";
+import { KeyLink, NavCard } from "@site/src/shared/ui";
 import { Hero } from "./hero";
 // FIXME: [FSDCUR] Replace to unified IBM/Carbon icons usage
 import IconNpm from "./npm.svg";
@@ -36,17 +36,62 @@ export default function HomePage() {
         <Layout title="Welcome" description="Architectural methodology for frontend projects">
             <Hero />
             <main>
-                <section className={styles.section}>
-                    <span className={styles.demoText}>
-                        Граф с профитами от использования: чистый гит, параллелизация работы,
-                        связность и зацепленность, общая терминология итд
-                    </span>
+                <section
+                    className={clsx(styles.section, styles.sectionText, styles.limitations)}
+                    id="scheme-limitations"
+                >
+                    <h2 className={styles.text}>
+                        Our aim is to cover the architectural needs of the{" "}
+                        <span className="text--primary">vast majority of front-end projects</span>
+                    </h2>
+                    <br />
+                    {/* FIXME: [FSDCUR] resolve color adaptive */}
+                    <img src="/img/promo/limitations.png" alt="themed--scheme" />
+                </section>
+                {/* Граф с профитами от использования: чистый гит, параллелизация работы, связность и зацепленность, общая терминология итд */}
+                <section className={clsx(styles.section, styles.sectionText, styles.profits)}>
+                    <h2 className={styles.text}>
+                        Less <span className="text--red">pain</span>, more{" "}
+                        <span className="text--green">gain</span>
+                    </h2>
+                    <div className={styles.profictsList}>
+                        <div className={styles.card}>
+                            <div className={styles.cardImage}>
+                                <img src="/img/promo/coupling.png" alt="coupling--scheme" />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <b>Low coupling, high cohesion</b>
+                                <p>You don’t need to remember what that means.</p>
+                                <p>
+                                    Just know that it makes your code{" "}
+                                    <span className="text--green">easier to unit-test</span> and
+                                    refactoring is much more{" "}
+                                    <span className="text--blue">predictable</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className={styles.card}>
+                            <div className={styles.cardImage}>
+                                <img src="/img/promo/structure.png" alt="structure--scheme" />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <b>Rigid, standardized structure</b>
+                                <p>New job where people use FSD? No stress!</p>
+                                <p>
+                                    You’ll <span className="text--green">feel right</span> at home
+                                    in the codebase. Get{" "}
+                                    <span className="text--blue">straight to business</span>, skip
+                                    the reinvented wheels.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </section>
                 <section className={styles.section}>
                     <img src="/img/promo/tech.png" alt="themed--scheme" />
                 </section>
                 <section className={styles.cols}>
-                    <div className={clsx(styles.col, styles.demoText)}>
+                    <div className={clsx(styles.col, styles.text)}>
                         {/* FIXME: [FSDCUR] refine layout */}
                         <span>
                             🍰 <b>Feature-Sliced Design</b> is not only a methodology, but also a
@@ -135,46 +180,50 @@ export default function HomePage() {
                         </div>
                     </div>
                 </section>
-                <section className={styles.section} id="scheme-limitations">
-                    <img src="/img/promo/limitations.png" alt="themed--scheme" />
-                </section>
-                <section className={styles.section}>
+                {/* <section className={styles.section}>
                     <img src="/img/promo/example-units.jpeg" alt="themed--scheme" />
+                </section> */}
+                <section className={clsx(styles.section, styles.sectionText, styles.companies)}>
+                    <h2 className={styles.text}>
+                        It’s <span className="text--red">dangerous</span> to go alone
+                    </h2>
+                    <p>
+                        But you don’t have to. These companies are already using Feature-Sliced
+                        Design:
+                    </p>
+                    <Companies />
                 </section>
-                <Companies />
-                <section className={clsx(styles.section, styles.cta)}>
-                    <span className={styles.demoText}>
-                        Take your frontend projects to the next level
-                        <br />
-                        with Feature-Sliced Design!
-                        <br />
-                        Dive in now 🚀
-                    </span>
+                <section className={clsx(styles.section, styles.sectionText, styles.nowar)}>
+                    <h2 className={styles.text}>
+                        The FSD team stands with Ukraine <div className={styles.flag} />
+                    </h2>
+                    <p>
+                        The Feature-Sliced Design team unanimously despises the invasion into
+                        Ukraine led by the Russian government.
+                    </p>
+                    <p>
+                        <span className="text--primary">Can and want to help?</span> The best way is
+                        to seek out a local organization that supports Ukrainian refugees in your
+                        country and help someone directly.
+                    </p>
+                </section>
+                <section className={clsx(styles.section, styles.sectionText, styles.cta)}>
+                    <h2 className={styles.text}>
+                        Think FSD and you are a match? <span className="text--red">&lt;3</span>
+                    </h2>
                     <div className={styles.ctaActions}>
-                        <NavCard
-                            theme="primary"
-                            Icon={ReadOutlined}
-                            title="Documentation"
-                            description="Learn the ins and outs of the Feature-Sliced Design methodology"
-                            primaryColor="red"
-                            to="/docs/"
-                        />
-                        <NavCard
-                            theme="primary"
-                            Icon={DeploymentUnitOutlined}
-                            title="Community"
-                            description="Discover a comprehensive ecosystem of tools and resources to support your frontend projects."
-                            primaryColor="green"
-                            to="/community"
-                        />
-                        <NavCard
-                            theme="primary"
-                            Icon={AppstoreAddOutlined}
-                            title="Showcase"
-                            description="Explore real-world examples and success stories using Feature-Sliced Design."
-                            primaryColor="blue"
-                            to="/showcase"
-                        />
+                        <KeyLink href="/docs" keyIcon={KeyLink.Enter} isActive>
+                            Dive into the docs
+                        </KeyLink>
+                        <KeyLink href="/docs" keyIcon="E">
+                            Discover ecosystem
+                        </KeyLink>
+                        <KeyLink href="/docs" keyIcon="S">
+                            Explore realworld examples
+                        </KeyLink>
+                        <KeyLink href="/docs" keyIcon="D">
+                            Talk to someone about it
+                        </KeyLink>
                     </div>
                 </section>
             </main>
