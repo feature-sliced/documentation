9a68676ea0d49d1976e03b00dbdc9313b769a014
Lev Chelyadinov <leva181777@gmail.com>
2024-09-27 22:20:44 +0200

    feat: rewrite and complete the migration guide from custom arch

--- config/docusaurus/routes.js
+++ config/docusaurus/routes.js
@@ -264,6 +264,17 @@ const LEGACY_ROUTES = [
             },
         ],
     },
+    {
+        group: "Rename 'legacy' to 'custom'",
+        details: "'Legacy' is derogatory, we don't get to call people's projects legacy",
+        children: [
+            {
+                title: "Rename 'legacy' to custom",
+                from: "/docs/guides/migration/from-legacy",
+                to: "/docs/guides/migration/from-custom",
+            }
+        ]
+    }
 ];
 
 // @returns { from, to }[]
