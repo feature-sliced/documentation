7eaa285925505bc5c874fbc190d1665ce444a50c
Lev Chelyadinov <leva181777@gmail.com>
2024-11-19 00:59:39 +0100

    Fix Stylelint configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
     extends: [
-        "stylelint-config-standard-scss",
         "stylelint-config-recommended",
+        "stylelint-config-standard-scss",
         "stylelint-config-recess-order",
     ],
     rules: {
         "color-hex-length": "long",
-        "at-rule-no-unknown": true,
         "selector-class-pattern": null,
     },
 };
