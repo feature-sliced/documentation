ffd56f49b6247f36d8e6705ac49a9bac31157ec8
Solant <runner62v6@gmail.com>
2026-01-30 00:28:53 +0100

    chore: initial astro setup

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
---
+++ src/content/docs/guides/example.md
@@ -0,0 +1,6 @@
+---
+title: Example Guide
+description: A guide in my new Starlight docs site.
+---
+
+# Hello
