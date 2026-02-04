9c604c898a15d6b3f54e2717e28889b5eee53f8c
Solant <runner62v6@gmail.com>
2026-02-05 00:29:42 +0100

    chore: ignore .astro

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/docs/get-started/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fget-started%2Foverview.mdx&astroContentModuleFlag=true")],
-["src/content/docs/get-started/faq.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fget-started%2Ffaq.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
-		
