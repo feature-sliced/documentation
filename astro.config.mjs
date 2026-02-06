da53d3975b814dad80b62314e083d6ce56bd07b7
Solant <runner62v6@gmail.com>
2026-02-06 17:21:01 +0100

    docs: add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Feature-Sliced Design',
+      favicon: './static/img/favicon/adaptive.svg',
       defaultLocale: 'root',
       customCss: [
         './src/styles/custom.css',
